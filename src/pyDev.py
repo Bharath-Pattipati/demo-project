48601a9d81fa19e7f2d1b68005b753b9590edd52
Bharath <bharathpattipati@gmail.com>
2024-12-21 20:28:32 -0500

    Create Developement Environment for Experiments

--- src/pyDev.py
+++ src/pyDev.py
@@ -1,9 +1,12 @@
+# %% import libraries
 import numpy as np
 import pandas as pd
 
+# %% print version
 print(np.__version__)
 print(pd.__version__)
 
+# %% Dataframe
 data = {
     "Name": ["John", "Anna", "Peter", "Linda"],
     "Age": [23, 36, 28, 45],
@@ -12,3 +15,8 @@ data = {
 
 df = pd.DataFrame(data)
 df = df.replace("New York", "New York City")
+
+# %% SVD
+X = np.random.rand(5, 3)  # Create random data matrix
+U, S, V = np.linalg.svd(X, full_matrices=True)  # Perform SVD
+Uhat, Shat, VThat = np.linalg.svd(X, full_matrices=False)  # Economy SVD
