b5d1865848abe00021326a9c5074a137a50668e7
Bharath <bharathpattipati@gmail.com>
2024-12-25 09:55:27 -0500

    Compressed sensing of two-tone cosine signal in pyOpt

--- src/pyOpt.py
+++ src/pyOpt.py
@@ -1,12 +1,18 @@
 # %% Import libraries
 import numpy as np
-from scipy.optimize import minimize
+
+# from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 import time
+from scipy.fftpack import dct, idct
+
+# CoSaMP: https://github.com/rfmiotto/CoSaMP
+# Original Needell and Tropp 2008 paper: https://arxiv.org/abs/0803.2392
+from cosamp.cosamp import cosamp
 
 # %% Sparse solutions to under-determined linear system.
 # Under-determined linear system: y = Theta * s i.e. more unknowns than knowns
-
+""" 
 tic = time.time()
 # Solve y = Theta * s for "s"
 n = 1000  # dimension of s
@@ -43,6 +49,62 @@ ax[1].hist(s_L1, bins=30)
 ax[1].set_xlabel("Value")
 ax[1].set_ylabel(r"Hist$(s_j)$")
 ax[1].set_title("Histogram of sparse solution")
+
+plt.tight_layout()
+plt.show() """
+
+# %% Recovering an Audio Signal from Sparse Measurements
+# Compressed sensing of two-tone cosine signal.
+
+tic = time.time()
+## Generate signal, DCT of signal
+n = 4096  # points in high resolution signal
+f1 = 100  # first-frequency (Hz)
+f2 = 500  # second-frequency (Hz)
+t = np.linspace(0, 1, n)
+x = np.cos(2 * f1 * np.pi * t) + np.cos(2 * f2 * np.pi * t)  # Two-tone signal
+xt = np.fft.fft(x)  # Fourier transformed signal
+PSD = xt * np.conj(xt) / n  # Power spectral density
+
+## Randomly sample signal
+# precise timing of the sparse measurements at a much higher resolution than
+# our sampling rate
+p = 128  # num. random samples, p = n/32
+perm = np.floor(np.random.rand(p) * n).astype(int)
+y = x[perm]
+
+## Solve compressed sensing problem
+Psi = dct(np.identity(n))  # Build discrete cosine transform basis
+Theta = Psi[perm, :]  # Measure rows of Psi
+
+# CS via matching pursuit
+s = cosamp(Theta, y, 10, tol=1e-10, max_iter=100)
+xrecon = idct(s)  # reconstruct full signal
+xr = np.fft.fft(x)  # Fourier transformed signal
+PSDr = xr * np.conj(xr) / n  # Power spectral density
+
+toc = time.time()  # End timer
+elapsed_time = toc - tic
+print(f"Elapsed time: {elapsed_time:.4f} seconds")
+
+# Create a line plot
+fig, ax = plt.subplots(2, 1, figsize=(10, 10))
+
+# Line plot in the first subplot
+ax[0].plot(x, label="Original Signal", color="blue")
+ax[0].plot(xrecon, label="Reconstructed Signal", color="red", linestyle="--")
+ax[0].legend()
+ax[0].set_xlabel("Time")
+ax[0].set_ylabel(r"X")
+ax[0].set_title("Compressed Sensing of Two-Tone Signal")
+
+ax[1].plot(PSD, label="Original Signal", color="blue")
+ax[1].plot(PSDr, label="Reconstructed Signal", color="red", linestyle="--")
+ax[1].legend()
+ax[1].set_xlabel("Frequency")
+ax[1].set_ylabel(r"PSD")
+ax[1].set_xlim(0, 1000)
+ax[1].set_title("Power Spectral Density")
 
 plt.tight_layout()
 plt.show()
