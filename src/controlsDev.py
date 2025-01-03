1b046aa5b4606fa8a61676a3d414cc21fa984ad1
Bharath <bharathpattipati@gmail.com>
2025-01-03 10:41:42 -0500

    Cruise Control System Example.

--- src/controlsDev.py
+++ src/controlsDev.py
@@ -6,10 +6,10 @@ from control.matlab import step, bode, nyquist, rlocus, logspace
 # import slycot
 
 import matplotlib.pyplot as plt
+import numpy as np
 
-# %% Initial trials
-# secord.py - demonstrate some standard MATLAB commands
-# Parameters defining the system
+# %% Second-order system example
+""" # Parameters defining the system
 m = 250.0  # system mass
 k = 40.0  # spring constant
 b = 60.0  # damping constant
@@ -42,4 +42,28 @@ rlocus(sys)
 plt.show(block=False)
 
 if "PYCONTROL_TEST_EXAMPLES" not in os.environ:
-    plt.show()
+    plt.show() """
+
+# %% Cruise Control System Example
+t = np.arange(0, 10, 0.01)  # time
+wr = 60 * np.ones_like(t)  # reference speed
+d = 10 * np.sin(np.pi * t)  # disturbance
+
+aModel = 1  # y = aModel*u
+aTrue = 0.5  # y = aTrue*u
+uOL = wr / aModel  # Open-loop u based on model
+yOL = aTrue * uOL + d  # Open-loop response
+
+K = 50  # control gain, u=K(wr-y)
+yCL = (aTrue * K / (1 + aTrue * K)) * wr + d / (1 + aTrue * K)  # Closed-loop response
+
+# plot reference, disturbance, open-loop response, and closed-loop response
+plt.figure(5)
+plt.plot(t, wr, label="Reference")
+plt.plot(t, d, label="Disturbance")
+plt.plot(t, yOL, label="Open-loop")
+plt.plot(t, yCL, label="Closed-loop")
+plt.legend()
+plt.xlabel("Time [s]")
+plt.ylabel("Speed [m/s]")
+plt.show()
