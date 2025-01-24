920570605c78cacbe19e7002acafec351ff82028
Bharath <bharathpattipati@gmail.com>
2025-01-24 00:15:44 -0500

    Performance improves as number of sub-domain integration points increases.

--- src/SINDyTool.py
+++ src/SINDyTool.py
@@ -16,8 +16,11 @@ PySINDy has been written to be as compatible with scikit-learn objects and metho
 import pysindy as ps
 from pysindy.utils import lorenz
 import numpy as np
-import pandas as pd
+
+# import pandas as pd
 from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
 
 # from pathlib import Path
 
@@ -103,7 +106,7 @@ x_test = solve_ivp(
 print("Model score: %f" % model.score(x_test, t=dt)) """
 
 # %% Test weak form ODE functionality on Lorenz equation
-# Generate measurement data
+""" # Generate measurement data
 dt = 0.002
 t_train = np.arange(0, 10, dt)
 t_train_span = (t_train[0], t_train[-1])
@@ -140,3 +143,67 @@ optimizer = ps.SR3(
 model = ps.SINDy(feature_library=ode_lib, optimizer=optimizer)
 model.fit(u_train)
 model.print()
+ """
+
+# %% Performance improves as number of sub-domain integration points increases
+# Generate measurement data
+dt = 0.001
+t_train = np.arange(0, 10, dt)
+t_train_span = (t_train[0], t_train[-1])
+u0_train = [-8, 8, 27]
+u0_test = [8, 7, 15]
+u_train = solve_ivp(
+    lorenz, t_train_span, u0_train, t_eval=t_train, **integrator_keywords
+).y.T
+u_test = solve_ivp(
+    lorenz, t_train_span, u0_test, t_eval=t_train, **integrator_keywords
+).y.T
+rmse = mean_squared_error(u_train, np.zeros((u_train).shape))
+u_dot_clean = ps.FiniteDifference()._differentiate(u_test, t=dt)
+u_clean = u_test
+u_train = u_train + np.random.normal(0, rmse / 5.0, u_train.shape)  # Add 20% noise
+rmse = mean_squared_error(u_test, np.zeros(u_test.shape))
+u_test = u_test + np.random.normal(0, rmse / 5.0, u_test.shape)  # Add 20% noise
+u_dot = ps.FiniteDifference()._differentiate(u_test, t=dt)
+
+# Same library terms as before
+# library_functions = [lambda x: x, lambda x: x * x, lambda x, y: x * y]
+# library_function_names = [lambda x: x, lambda x: x + x, lambda x, y: x + y]
+
+# Scan over the number of integration points and the number of subdomains
+n = 10
+errs = np.zeros((n))
+K_scan = np.linspace(20, 2000, n, dtype=int)
+for i, K in enumerate(K_scan):
+    ode_lib = ps.WeakPDELibrary(
+        library_functions=[lambda x: x, lambda x: x**2, lambda x, y: x * y],
+        spatiotemporal_grid=t_train,
+        include_bias=True,
+        is_uniform=True,
+        K=K,
+    )
+    opt = ps.SR3(
+        threshold=0.05,
+        thresholder="l0",
+        max_iter=1000,
+        normalize_columns=True,
+        tol=1e-1,
+    )
+    u_dot_train_integral = ode_lib.convert_u_dot_integral(u_train)
+
+    # Instantiate and fit the SINDy model with the integral of u_dot
+    model = ps.SINDy(feature_library=ode_lib, optimizer=opt)
+    model.fit(u_train)
+    errs[i] = np.sqrt(
+        (
+            np.sum((u_dot_train_integral - opt.Theta_ @ opt.coef_.T) ** 2)
+            / np.sum(u_dot_train_integral**2)
+        )
+        / u_dot_train_integral.shape[0]
+    )
+
+plt.title("Convergence of weak SINDy, hyperparameter scan", fontsize=12)
+plt.plot(K_scan, errs)
+plt.xlabel("Number of subdomains", fontsize=16)
+plt.ylabel("Error", fontsize=16)
+plt.show()
