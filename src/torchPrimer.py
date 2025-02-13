2d7ddb14df8a6100ca508c2523823e2276fda7a7
Bharath <bharathpattipati@gmail.com>
2025-02-12 21:45:28 -0500

    Multi-Head Attention Class.

--- src/torchPrimer.py
+++ src/torchPrimer.py
@@ -685,7 +685,7 @@ print(
 
 
 # %% Self-Attention Class
-class SelfAttention(nn.Module):
+""" class SelfAttention(nn.Module):
     def __init__(self, d_model=2, row_dim=0, col_dim=1):
         ## d_model = the number of embedding values per token.
         ##           Because we want to be able to do the math by hand, we've
@@ -727,11 +727,11 @@ class SelfAttention(nn.Module):
         # Scale the values by their associated percentages and add them up
         attention_scores = torch.matmul(attention_percents, v)
 
-        return attention_scores
+        return attention_scores """
 
 
 # %% Calculate Self-Attention
-## create a matrix of token encodings...
+""" ## create a matrix of token encodings...
 encodings_matrix = torch.tensor([[1.16, 0.23], [0.57, 1.36], [4.41, -2.16]])
 
 ## set the seed for the random number generator
@@ -741,11 +741,11 @@ torch.manual_seed(42)
 selfAttention = SelfAttention(d_model=2, row_dim=0, col_dim=1)
 
 ## calculate basic attention for the token encodings
-selfAttention(encodings_matrix)
+selfAttention(encodings_matrix) """
 
 # %% Verify Calculations
 ## print out the weight matrix that creates the queries
-selfAttention.W_q.weight.transpose(0, 1)
+""" selfAttention.W_q.weight.transpose(0, 1)
 
 ## print out the weight matrix that creates the keys
 selfAttention.W_k.weight.transpose(0, 1)
@@ -777,11 +777,11 @@ scaled_sims
 attention_percents = F.softmax(scaled_sims, dim=1)
 attention_percents
 
-torch.matmul(attention_percents, selfAttention.W_v(encodings_matrix))
+torch.matmul(attention_percents, selfAttention.W_v(encodings_matrix)) """
 
 
 # %% Masked Self-Attention
-class MaskedSelfAttention(nn.Module):
+""" class MaskedSelfAttention(nn.Module):
     def __init__(self, d_model=2, row_dim=0, col_dim=1):
         super().__init__()
 
@@ -817,11 +817,11 @@ class MaskedSelfAttention(nn.Module):
         # Scale the values by their associated percentages and add them up
         attention_scores = torch.matmul(attention_percents, v)
 
-        return attention_scores
+        return attention_scores """
 
 
 # %% Calculate Masked Self-Attention and Verify Calculations
-## create a matrix of token encodings...
+""" ## create a matrix of token encodings...
 encodings_matrix = torch.tensor([[1.16, 0.23], [0.57, 1.36], [4.41, -2.16]])
 
 ## set the seed for the random number generator
@@ -878,5 +878,100 @@ masked_scaled_sims
 attention_percents = F.softmax(masked_scaled_sims, dim=1)
 attention_percents
 
-torch.matmul(attention_percents, maskedSelfAttention.W_v(encodings_matrix))
+torch.matmul(attention_percents, maskedSelfAttention.W_v(encodings_matrix)) """
+
+
+# %% Baseline Self-Attention Class
+class Attention(nn.Module):
+    def __init__(self, d_model=2, row_dim=0, col_dim=1):
+        super().__init__()
+
+        self.W_q = nn.Linear(in_features=d_model, out_features=d_model, bias=False)
+        self.W_k = nn.Linear(in_features=d_model, out_features=d_model, bias=False)
+        self.W_v = nn.Linear(in_features=d_model, out_features=d_model, bias=False)
+
+        self.row_dim = row_dim
+        self.col_dim = col_dim
+
+    ## The only change from SelfAttention and attention is that
+    ## now we expect 3 sets of encodings to be passed in...
+    def forward(self, encodings_for_q, encodings_for_k, encodings_for_v, mask=None):
+        ## ...and we pass those sets of encodings to the various weight matrices.
+        q = self.W_q(encodings_for_q)
+        k = self.W_k(encodings_for_k)
+        v = self.W_v(encodings_for_v)
+
+        sims = torch.matmul(q, k.transpose(dim0=self.row_dim, dim1=self.col_dim))
+
+        scaled_sims = sims / torch.tensor(k.size(self.col_dim) ** 0.5)
+
+        if mask is not None:
+            scaled_sims = scaled_sims.masked_fill(mask=mask, value=-1e9)
+
+        attention_percents = F.softmax(scaled_sims, dim=self.col_dim)
+
+        attention_scores = torch.matmul(attention_percents, v)
+
+        return attention_scores
+
+
+# %% Encoder-Decoder Attention
+## create matrices of token encodings...
+encodings_for_q = torch.tensor([[1.16, 0.23], [0.57, 1.36], [4.41, -2.16]])
+
+encodings_for_k = torch.tensor([[1.16, 0.23], [0.57, 1.36], [4.41, -2.16]])
+
+encodings_for_v = torch.tensor([[1.16, 0.23], [0.57, 1.36], [4.41, -2.16]])
+
+## set the seed for the random number generator
+torch.manual_seed(42)
+
+## create an attention object
+attention = Attention(d_model=2, row_dim=0, col_dim=1)
+
+## calculate encoder-decoder attention
+attention(encodings_for_q, encodings_for_k, encodings_for_v)
+
+
+# %% Multi-Head Attention
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_model=2, row_dim=0, col_dim=1, num_heads=1):
+        super().__init__()
+
+        ## create a bunch of attention heads
+        self.heads = nn.ModuleList(
+            [Attention(d_model, row_dim, col_dim) for _ in range(num_heads)]
+        )
+
+        self.col_dim = col_dim
+
+    def forward(self, encodings_for_q, encodings_for_k, encodings_for_v):
+        ## run the data through all of the attention heads
+        return torch.cat(
+            [
+                head(encodings_for_q, encodings_for_k, encodings_for_v)
+                for head in self.heads
+            ],
+            dim=self.col_dim,
+        )
+
+
+# %% Verify Multi-head attention
+## set the seed for the random number generator
+torch.manual_seed(42)
+
+## create an attention object
+multiHeadAttention = MultiHeadAttention(d_model=2, row_dim=0, col_dim=1, num_heads=1)
+
+## calculate encoder-decoder attention
+multiHeadAttention(encodings_for_q, encodings_for_k, encodings_for_v)
+
+## set the seed for the random number generator
+torch.manual_seed(42)
+
+## create an attention object
+multiHeadAttention = MultiHeadAttention(d_model=2, row_dim=0, col_dim=1, num_heads=2)
+
+## calculate encoder-decoder attention
+multiHeadAttention(encodings_for_q, encodings_for_k, encodings_for_v)
 # %%
